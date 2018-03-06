e75cfc8fd36e32e19a5fc739ea040d75da21f973
freeitgroupe <freeitgroupe@gmail.com>
2018-03-06 16:10:29 +0200

    start project

---
+++ config/init.php
@@ -0,0 +1,11 @@
+<?php
+define("DEBUG",1);//1-err on, 0-err off
+define("ROOT",dirname(__DIR__));//корень приложения
+define("WWW",ROOT . "/public");//публичная папка
+define("APP",ROOT . "/app");//МВС файлы
+define("CORE",ROOT . "/vendor/ishop/core");//Ядро
+define("LIBS",ROOT . "/vendor/ishop/core/libs");
+define("CACHE",ROOT . "/tmp/cache");
+define("CONF",ROOT . "/config");
+define("LAYOUT","default");
+
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 06.03.2018
+ * Time: 14:01
+ */
+require_once dirname(__DIR__) . '/config/init.php';
+echo dirname(__DIR__);
+//var_dump($_SERVER['QUERY_STRING']);
