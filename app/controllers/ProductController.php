eab30286a4cd22062d6cdc1a4e6bf2103e4e3547
freeitgroupe <freeitgroupe@gmail.com>
2018-08-03 14:49:59 +0300

    03082018

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -7,15 +7,26 @@ class ProductController extends AppController
     public function viewAction(){
         $alias = $this->route['alias'];
         $get_one_product = \R::findOne('products', "alias = ? AND mark_view = '1'", [$alias]);
-        $id = $get_one_product->id;
-        //$product_rating = \R::findOne('prod_rating', "product_id = {$get_one_product->id} AND status = 1");
-        $sql = 'SELECT rating_number, rating_id as id, FORMAT((total_points / rating_number),1) as average_rating FROM prod_rating WHERE product_id =' . $id . ' AND status = 1 ';
-        $rows = \R::getAll($sql);
-        $product_rating = \R::convertToBeans('prod_rating',$rows);
 
-        debug($product_rating);
         if(!$get_one_product){
             throw  new \Exception('Page not found',404);
+        }
+
+        //Рейтинг товара
+        $id = $get_one_product->id;
+        $sql = 'SELECT rating_number, rating_id as id, FORMAT((total_points / rating_number),1) as average_rating FROM prod_rating WHERE product_id =' . $id . ' AND status = 1 ';
+        $rows = \R::getAll($sql);
+        $resProductRating = \R::convertToBeans('prod_rating',$rows);
+
+        $product_rating = [];
+        if(count($resProductRating) >= 1){
+            foreach ($resProductRating as $v){
+                $product_rating['average_rating'] = $v->average_rating;
+                $product_rating['rating_number'] = $v->rating_number;
+            }
+        }else{
+            $product_rating['average_rating'] = 0;
+            $product_rating['rating_number'] = 0;
         }
 
         //хлебные крошки
@@ -30,9 +41,6 @@ class ProductController extends AppController
 
         //модификация
 
-        //рейтинг товара
-
-        //
 
 
         $this->setMeta($get_one_product->title, $get_one_product->meta_d, $get_one_product->meta_k);
